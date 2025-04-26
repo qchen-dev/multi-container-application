de6a296ca6189d7991349169d91ea1ab6d885d04
nick chen <qche.au@gmail.com>
2025-04-26 18:13:42 +1000

    update allow access from host windows to vm react web container

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['client'], // Add 'client' to the allowed hosts list
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['client'],
+    hmr: {
+      port: 3080,
+      clientPort: 3080,
+      protocol: 'ws',
+      path: '/ws',
+    },
   },
 });
