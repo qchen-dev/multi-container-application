f17c02fee0a97cccffd8d405855c84cff763e500
nick chen <qche.au@gmail.com>
2025-04-27 12:32:01 +1000

    update typo postgres database key

--- server/keys.js
+++ server/keys.js
@@ -3,7 +3,7 @@ module.exports = {
   redisPort: process.env.REDIS_PORT,
   postgresUser: process.env.POSTGRES_USER,
   postgresHost: process.env.POSTGRES_HOST,
-  postgresDatabase: process.env.POSTGRES_HOST,
+  postgresDatabase: process.env.POSTGRES_DATABASE,
   postgresPassword: process.env.POSTGRES_PASSWORD,
   postgresPort: process.env.POSTGRES_PORT,
 };
